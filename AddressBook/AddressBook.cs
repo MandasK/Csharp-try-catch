998daa81705432655e2d5e55f78b9173169f6d0f
MandasK <king.amanda.r@gmail.com>
2020-08-27 13:59:41 -0500

    AddressBook, Contact classes add and try/catches are passing

---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace AddressBookApp
+{
+    public class AddressBook
+    {
+        Dictionary<string, Contact> Contacts = new Dictionary<string, Contact>();
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact.Email, contact);
+        }
+        public Contact GetByEmail(string email)
+        {
+            {
+                return Contacts[email];
+            }
+        }
+    }
+
+}
